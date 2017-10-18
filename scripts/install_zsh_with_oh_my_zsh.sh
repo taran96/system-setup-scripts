ff8d53e9e88d09ed5d18f7fc5b17c28154a4056c
Taranvir Waraich <taranvir@me.com>
2017-10-18 16:16:04 -0400

    Renamed vim-plug to vim-extra and added zsh install

---
+++ scripts/install_zsh_with_oh_my_zsh.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+_RELATIVE_PATH=`dirname $BASH_SOURCE`
+SCRIPTPATH=$( cd $_RELATIVE_PATH && pwd -P )
+
+. $SCRIPTPATH/colors.sh
+. $SCRIPTPATH/helpers.sh
+
+
+if [ "$(whoami)" != "root" ]
+then
+    sudo su -s "$0"
+    exit
+fi
+
+printf "${PADDING}\n"
+printf "${ORANGE}Installing zsh with oh_my_zsh!${NC}\n"
+printf "${BORDER}\n"
+
+printf "\n${YELLOW}WARNING: oh_my_zsh's install script starts another zsh session within the current session so exit session when the prompt comes up while still installing${NC}\n"
+run_command "Updating sources" "sudo apt update"
+run_command "Installing zsh" "sudo apt install --yes zsh"
+sudo su $SUDO_USER
+cd /tmp
+run_command "Downloading oh_my_zsh" "curl -o install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
+run_command "Make install script executable" "sudo chmod +x install.sh"
+run_command "Installing oh_my_zsh for ${SUDO_USER}" './install.sh'
+
+printf "${PADDING}\n"
+
+
