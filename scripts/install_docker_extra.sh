a9c70298da02de5a9ef4f26cfc47efa60020603d
Taranvir Waraich <taranvir@me.com>
2017-10-19 11:03:10 -0400

    Added to docker install extra

--- scripts/install_docker_extra.sh
+++ scripts/install_docker_extra.sh
@@ -11,12 +11,16 @@ then
     sudo echo 
 fi
 
+DOCKER_CONFIG_FILE="${HOME}/.docker/config.json"
+
 printf "${PADDING}\n"
 printf "${ORANGE}Enabling access to Docker for ${USER} ${NC}\n"
 printf "${BORDER}\n"
 
 
+
 run_command "Adding ${USER} to the docker group" "sudo usermod -aG docker ${USER}"
+run_command "Adding ${USER} as owner of ${DOCKER_CONFIG_FILE}" "sudo chown ${USER} ${DOCKER_CONFIG_FILE}"
 printf "${YELLOW}\nYou need to logout and then login for the changes to be applied${NC}\n"
 
 printf "${PADDING}\n"
